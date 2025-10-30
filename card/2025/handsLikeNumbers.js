af0e3bfe1e908fa5b1bdfc21d2a3ed778701cfa3
thetrev68 <thetrev68@hotmail.com>
2025-10-30 15:58:32 -0500

    data: build 2025 NMJL Card Data (in progress)

---
+++ card/2025/handsLikeNumbers.js
@@ -0,0 +1,118 @@
+import {SUIT} from "../../constants.js";
+
+// Rules for describing hands
+//  - put exact singles (non-virtual suit) first in components array
+//      (E.g. single white dragon used as 0)
+
+export const handsLikeNumbers = {
+    groupDescription: "LikeNumbers",
+    hands: [
+        {
+            description: "FF 1111 D 1111 D 11 (Any 3 Suits)",
+            vsuitCount: 3,
+            concealed: false,
+            odd: false,
+            even: false,
+            components: [
+                {
+                    suit: SUIT.FLOWER,
+                    number: 0,
+                    count: 2
+                },
+                {
+                    suit: SUIT.VSUIT1,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 4
+                },
+                {
+                    suit: SUIT.VSUIT1_DRAGON,
+                    number: 0,
+                    count: 1
+                },
+                {
+                    suit: SUIT.VSUIT2,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 4
+                },
+                {
+                    suit: SUIT.VSUIT2_DRAGON,
+                    number: 0,
+                    count: 1
+                },
+                {
+                    suit: SUIT.VSUIT3,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 2
+                }
+            ]
+        },
+        {
+            description: "FFFF 11 111 111 11 (Any 3 Suits. Pairs Must Be Same Suit)",
+            vsuitCount: 3,
+            concealed: false,
+            odd: false,
+            even: false,
+            components: [
+                {
+                    suit: SUIT.FLOWER,
+                    number: 0,
+                    count: 4
+                },
+                {
+                    suit: SUIT.VSUIT1,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 2
+                },
+                {
+                    suit: SUIT.VSUIT2,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 3
+                },
+                {
+                    suit: SUIT.VSUIT3,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 3
+                },
+                {
+                    suit: SUIT.VSUIT1,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 2
+                }
+            ]
+        },
+        {
+            description: "FF 111 111 111 DDD (Any 3 Suits. Any Dragon)",
+            vsuitCount: 3,
+            concealed: true,
+            odd: false,
+            even: false,
+            components: [
+                {
+                    suit: SUIT.FLOWER,
+                    number: 0,
+                    count: 2
+                },
+                {
+                    suit: SUIT.VSUIT1,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 3
+                },
+                {
+                    suit: SUIT.VSUIT2,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 3
+                },
+                {
+                    suit: SUIT.VSUIT3,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 3
+                },
+                {
+                    suit: SUIT.VSUIT1_DRAGON,
+                    number: 0,
+                    count: 3
+                }
+            ]
+        }
+    ]
+};
---
+++ card/2025/handsQuints.js
@@ -0,0 +1,98 @@
+import {SUIT, WIND} from "../../constants.js";
+
+// Rules for describing hands
+//  - put exact singles (non-virtual suit) first in components array
+//      (E.g. single white dragon used as 0)
+
+export const handsQuints = {
+    groupDescription: "Quints",
+    hands: [
+        {
+            description: "FF 111 2222 33333 (Any 3 Suits, Any 3 Consec. Nos)",
+            vsuitCount: 3,
+            concealed: false,
+            odd: false,
+            even: false,
+            components: [
+                {
+                    suit: SUIT.FLOWER,
+                    number: 0,
+                    count: 2
+                },
+                {
+                    suit: SUIT.VSUIT1,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 3
+                },
+                {
+                    suit: SUIT.VSUIT2,
+                    number: VNUMBER_CONSECUTIVE2,
+                    count: 4
+                },
+                {
+                    suit: SUIT.VSUIT3,
+                    number: VNUMBER_CONSECUTIVE3,
+                    count: 5
+                }
+            ]
+        },
+        {
+            description: "11 111 NNNN 22222 (Any 1 Suit, Any Consec. Nos, Any Wind)",
+            vsuitCount: 1,
+            concealed: false,
+            odd: false,
+            even: false,
+            components: [
+                {
+                    suit: SUIT.VSUIT1,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 2
+                },
+                {
+                    suit: SUIT.VSUIT1,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 3
+                },
+                {
+                    suit: SUIT.WIND,
+                    number: WIND.NORTH,
+                    count: 4
+                },
+                {
+                    suit: SUIT.VSUIT1,
+                    number: VNUMBER_CONSECUTIVE2,
+                    count: 5
+                }
+            ]
+        },
+        {
+            description: "FF 11111 11 11111 (Any 3 Suits, Any Like Nos)",
+            vsuitCount: 3,
+            concealed: false,
+            odd: false,
+            even: false,
+            components: [
+                {
+                    suit: SUIT.FLOWER,
+                    number: 0,
+                    count: 2
+                },
+                {
+                    suit: SUIT.VSUIT1,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 5
+                },
+                {
+                    suit: SUIT.VSUIT2,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 2
+                },
+                {
+                    suit: SUIT.VSUIT3,
+                    number: VNUMBER_CONSECUTIVE1,
+                    count: 5
+                }
+            ]
+        }
+    ]
+};
